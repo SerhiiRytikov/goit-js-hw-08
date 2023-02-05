59c3789d4350a18f7baff44e203dccb6d5cb4b79
SerhiiRytikov <serg_ritikov@ukr.net>
2023-02-05 18:57:47 +0200

    1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,25 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
-// Change code below this line
+import SimpleLightbox from 'simplelightbox';
+// Додатковий імпорт стилів
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
 console.log(galleryItems);
+
+const galleryAllPhotoPreview = document.querySelector('.gallery');
+
+const gelImg = galleryItems
+  .map(
+    ({ preview, original, description }) =>
+      `<a class="gallery__item" href=${original}>
+      <img class="gallery__image" src=${preview} 
+      alt=${description} /></a>`
+  )
+  .join('');
+
+galleryAllPhotoPreview.innerHTML = gelImg;
+
+let gallery = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});

dc4c287f44effcc046314ef09d8ff41d1d344499
SerhiiRytikov <serg_ritikov@ukr.net>
2023-02-06 16:44:49 +0200

    1

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,17 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+player.on(
+  'timeupdate',
+  throttle(function (event) {
+    localStorage.setItem('videoplayer-current-time', event.seconds);
+  }, 1000)
+);
+const result = localStorage.getItem('videoplayer-current-time');
+
+player.setCurrentTime(Number(result)).then(function (seconds) {
+  // seconds = the actual time that the player seeked to
+});
